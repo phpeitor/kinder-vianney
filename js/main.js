9480e57668b1afa3015fe76f5358998db7907343
Dr. PHP <alex1991usat@gmail.com>
2026-04-11 14:54:01 -0500

    feat: responsive slider

--- js/main.js
+++ js/main.js
@@ -408,8 +408,8 @@ document.addEventListener("DOMContentLoaded", function () {
     } else {
       lsjQuery("#layerslider_1").layerSlider({
         sliderVersion: "",
-        responsiveUnder: 1170,
-        layersContainer: 1170,
+        responsiveUnder: 0,
+        layersContainer: 0,
         hideUnder: 0,
         hideOver: 100000,
         pauseOnHover: "on",
